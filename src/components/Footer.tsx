2b465c2b7857f9f11687dcdeb98c3bf1bcfcc288
simaraconceicao <contato@simaraconceicao.com>
2022-08-16 12:02:01 -0300

    insere footer e cria responsividade pra página subcriber

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export function Footer() {
+  return(
+    <div className="flex justify-center flex-col items-center">
+      <p className="max-w-[370px] m-10 text-gray-200 text-xl leading-relaxed text-center lg:max-w-4xl">
+        Feito com ♡ por Simara | 
+        Criei essa plataforma durante os estudos no evento IgniteLab da Rocketseat.
+          <strong className="bg-blue-500">
+            <a href="https://github.com/simaraconceicao/quero-ser-dev-plataform" target="_blank">Inclusive você pode acessar o código dessa plataforma aqui no meu github.</a> 
+          </strong>
+      </p>
+    </div>
+  )
+}
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Logo } from '../components/Logo'
+import { Footer } from '../components/Footer'
 import { useCreateSubscriberMutation } from '../graphql/generated'
 
 export function Subscribe() {
@@ -25,45 +26,46 @@ export function Subscribe() {
   }
 
   return(
-    <div className="bg-blur min-h-screen bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mx-auto mb-20">
-        <div className="max-w-[640px]">
-          <Logo />
-          <h1 className="text-[2.5rem] leading-tight">Quero Ser Dev | Conteúdos de <strong className="bg-blue-500">migração de carreira</strong> para tecnologia e <strong className="bg-blue-500">tutoriais</strong> para iniciantes</h1>
-          <p className="mt-4 text-gray-200 leading-relaxed">
-            Criei essa plataforma durante os estudos no evento IgniteLab da Rocketseat.
-            <strong className="bg-blue-500">
-              <a href="https://github.com/simaraconceicao/quero-ser-dev-plataform" target="_blank">Inclusive você pode acessar o código dessa plataforma aqui no meu github.</a> 
-            </strong>
-            Aqui eu pretendo disponibilizar conteúdos não listados e exclusivos do canal quero ser dev com tutoriais para pessoas iniciantes. Para acessar basta se cadastrar ao lado.
-          </p>
-        </div>
-        <div className="p-8 bg-gray-700 border border-gray-300 rounded">
-          <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
-          <form onSubmit={handleSubscribe} className="flex flex-col w-full gap-2">
-            <input 
-              className="bg-gray-900 rounded px-5 h-14"
-              type="text" 
-              placeholder="Seu nome completo" 
-              onChange={event => setName(event.target.value)}
-            />
-            <input 
-              className="bg-gray-900 rounded px-5 h-14"
-              type="email" 
-              placeholder="Digite seu e-mail" 
-              onChange={event => setEmail(event.target.value)}
-            />
+    <>
+      <div className="bg-blur min-h-screen bg-cover bg-no-repeat flex flex-col items-center lg:flex lg:justify-center lg:items-center lg:flex-row">
+        <div className="flex justify-center flex-col items-center lg:flex lg:justify-center lg:items-center lg:flex-row lg:gap-72">
+          <div className="max-w-[768px] flex justify-center flex-col items-center">
+            <Logo />
+            <h1 className="max-w-[350px] text-center text-5xl font-bold lg:max-w-[768px]">
+              Tutoriais de ReactJS para iniciantes 
+            </h1>
+            <p className="max-w-[370px] mt-4 mb-10 text-gray-200 text-2xl leading-relaxed text-center p-6 lg:max-w-[768px]">
+              Conteúdos não listados e exclusivos do canal quero ser dev com tutoriais para pessoas iniciantes. Para acessar basta se cadastrar.
+            </p>
+          </div>
+          <div className="p-8 bg-gray-700 border border-gray-300 rounded mb-[20px]">
+            <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+            <form onSubmit={handleSubscribe} className="flex flex-col w-full gap-2">
+              <input 
+                className="bg-gray-900 rounded px-5 h-14"
+                type="text" 
+                placeholder="Seu nome completo" 
+                onChange={event => setName(event.target.value)}
+              />
+              <input 
+                className="bg-gray-900 rounded px-5 h-14"
+                type="email" 
+                placeholder="Digite seu e-mail" 
+                onChange={event => setEmail(event.target.value)}
+              />
 
-            <button 
-              type="submit"
-              disabled={loading}
-              className="mt-4 bg-blue-500 uppercase py-4 rounded font-bold text-sm hover:bg-blue-700 transition-colors disabled:opacity-50"
-            >
-              Acessar o conteúdo
-            </button>
-          </form>
+              <button 
+                type="submit"
+                disabled={loading}
+                className="mt-4 bg-blue-500 uppercase py-4 rounded font-bold text-sm hover:bg-blue-700 transition-colors disabled:opacity-50"
+              >
+                Acessar o conteúdo
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  )
+      <Footer />
+    </>
+  ) 
 }
