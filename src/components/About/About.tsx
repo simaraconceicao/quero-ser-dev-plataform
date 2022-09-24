146657c6a2b869d5ba9d308d6442b87d73a39bc8
simaraconceicao <contato@simaraconceicao.com>
2022-09-24 03:21:23 -0300

    revisao de copy

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -64,16 +64,21 @@ export function About() {
   return (
     <>
       <div>
-        <div className="w-[100%] flex justify-center flex-col items-center lg:flex lg:justify-center lg:items-center lg:flex-col lg:gap-1">
+        <div className="w-[100%] flex justify-center flex-col items-center lg:flex lg:justify-center lg:items-center lg:flex-col">
           <span className="text-center font-bold text-4xl m-10 border-gray-400 flex justify-center">
             Conteúdo do curso ReactJS para iniciantes
           </span>
-          <p className="w-[70%] mt-4 mb-10 text-gray-200 text-2xl leading-relaxed text-center flex justify-center p-6">
+          <p className="w-[70%] mt-4 mb-1 text-gray-200 text-2xl leading-relaxed text-center flex justify-center p-6">
             ReactJS é a tecnologia mais usada por pessoas desenvolvedoras experientes e também por pessoas que estão aprendendo a programar, de acordo com a pesquisa do StackOverflow com mais de 70mil pessoas da comunidade tech lançada em maio de 2022.
-            Imagina você aprendendo os passos iniciais de como as grandes plataformas web são criadas. Plataformas como Facebook, Instagram, Twitter, TikTok, Netflix, AirBnB, Notion e muitas outras.
-            Isso te abre possibilidades de conseguir sua vaga como dev front-end jr., no mercado que mais cresce no Brasil e no mundo, com salários e benefícios atrativos até mesmo para pessoas iniciantes ou estagiárias.
-            Se liga aqui embaixo, como tá irado e super completo o conteúdo do curso:
           </p>
+          <p className="w-[70%] mt-4 mb-2 text-gray-200 text-2xl leading-relaxed text-center flex justify-center p-6">
+            Imagina você aprendendo os passos iniciais de como as grandes plataformas web são criadas nos dias de hoje. Plataformas como Facebook, Instagram, Twitter, TikTok, Netflix, AirBnB, Notion e muitas outras.
+            Isso te abre possibilidades de conseguir sua vaga como dev front-end jr., num dos mercados que mais cresce no Brasil e no mundo, com salários e benefícios atrativos até mesmo para pessoas iniciantes ou estagiárias.
+          </p>
+          <span className="text-center font-bold text-2xl m-10 border-gray-400 flex justify-center">
+            Se liga aqui embaixo, como tá irado e super completo o conteúdo do curso:
+          </span>
+
         </div>
         
         <div className={styles.containerCard}>
